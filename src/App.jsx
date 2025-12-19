c45187e462ec7447d9d0ffe8c897589b683afe12
Kaniz-FS <ravenclaw.k12@gmail.com>
2025-12-20 01:48:02 +0600

    go

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+// connecting components
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Container from './components/Container';
+import vectorImg from "./assets/vectorF.png"
+import vectorImag from "./assets/vectorR.png"
+
+
+function App() {
+  return (
+    <>
+<NavBar></NavBar>
+<Container>
+  <div className='grid grid-cols-2 gap-6 my-15 px-8'>
+    {/* left col */}
+  <div className='rounded-md relative  min-h-[250px] overflow-hidden'>
+     
+      <div className="absolute inset-0 bg-linear-to-r from-[#632EE3] to-[#9F62F2]"></div>
+{/* left side */}
+      <div
+    className="absolute left-0 top-0 w-[45%] h-full bg-no-repeat  bg-left"
+    style={{ backgroundImage: `url(${vectorImg})` }}
+  ></div>
+
+  {/* right side */}
+  <div
+    className="absolute right-0 top-0 w-[45%] h-full bg-no-repeat bg-right "
+    style={{ 
+      backgroundImage: `url(${vectorImg})`, 
+      transform: 'scaleX(-1)' 
+    }}
+  ></div>
+  {/* text */}
+  <div className="absolute inset-0 flex flex-col items-center justify-center z-30 text-white">
+    <h2 className='font-semibold text-3xl'>In-Progress</h2>
+    <p className='font-semibold text-2xl'>0</p>
+  </div>
+  </div>
+  {/* right col */}
+ <div className='rounded-md relative  min-h-[250px] overflow-hidden' 
+  >
+    <div className="absolute inset-0 bg-linear-to-r from-[#54CF68] to-[#00827A]"></div>
+{/* left side */}
+      <div
+    className="absolute left-0 top-0 w-[45%] h-full bg-no-repeat  bg-left"
+    style={{ backgroundImage: `url(${vectorImg})` }}
+  ></div>
+
+  {/* right side */}
+  <div
+    className="absolute right-0 top-0 w-[45%] h-full bg-no-repeat bg-right "
+    style={{ 
+      backgroundImage: `url(${vectorImg})`, 
+      transform: 'scaleX(-1)' 
+    }}
+  ></div>
+  {/* text */}
+  <div className="absolute inset-0 flex flex-col items-center justify-center z-30 text-white">
+    <h2 className='font-semibold text-3xl'>Resolved</h2>
+    <p className='font-semibold text-2xl'>0</p>
+  </div>
+  </div>
+</div>
+</Container>
+<Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import Container from './Container';
+
+
+
+const NavBar =()=>{
+    return (
+    
+
+  <div className=" bg-base-100 shadow-sm">
+  <Container>
+  <nav className='px-8 py-4 flex justify-between item-center'>
+    <h1 className="text-2xl font-semibold">CS — Ticket System</h1>
+ 
+  <div className="flex items-center gap-6">
+    <div className='flex gap-5 font-medium'>
+      <a>Home</a>
+      <a>FAQ</a>
+      <a>Changelog</a>
+      <a>Blog</a>
+      <a>Download</a>
+      <a>Contact</a>
+    </div>
+    <a className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white">+ New Ticket</a>
+  </div>
+  </nav>
+ </Container>
+</div>
+
+
+   );
+};
+
+export default NavBar;
