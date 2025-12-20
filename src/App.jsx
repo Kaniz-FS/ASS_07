ad2eb41c466040e240a83f0e4e25766453314352
Kaniz-FS <ravenclaw.k12@gmail.com>
2025-12-20 14:13:58 +0600

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,26 @@
+import './App.css';
 // connecting components
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-<<<<<<< HEAD
-import Container from './components/Container';
-import vectorImg from "./assets/vectorF.png"
+import IssuesManagement from './components/IssuesManagement';
 
-=======
->>>>>>> d49f98647d7159136ac108c7792890e0ebd23053
+import Container from './components/Container';
+
 
 
 function App() {
   return (
     <>
 <NavBar></NavBar>
-<<<<<<< HEAD
-<Container>
-  <div className='grid grid-cols-2 gap-6 my-15 px-8'>
-    {/* left col */}
-  <div className='rounded-md relative  min-h-[250px] overflow-hidden'>
-     
-      <div className="absolute inset-0 bg-linear-to-r from-[#632EE3] to-[#9F62F2]"></div>
-{/* left side */}
-      <div
-    className="absolute left-0 top-0 w-[45%] h-full bg-no-repeat  bg-left"
-    style={{ backgroundImage: `url(${vectorImg})` }}
-  ></div>
 
-  {/* right side */}
-  <div
-    className="absolute right-0 top-0 w-[45%] h-full bg-no-repeat bg-right "
-    style={{ 
-      backgroundImage: `url(${vectorImg})`, 
-      transform: 'scaleX(-1)' 
-    }}
-  ></div>
-  {/* text */}
-  <div className="absolute inset-0 flex flex-col items-center justify-center z-30 text-white">
-    <h2 className='font-semibold text-3xl'>In-Progress</h2>
-    <p className='font-semibold text-2xl'>0</p>
-  </div>
-  </div>
-  {/* right col */}
- <div className='rounded-md relative  min-h-[250px] overflow-hidden' 
-  >
-    <div className="absolute inset-0 bg-linear-to-r from-[#54CF68] to-[#00827A]"></div>
-{/* left side */}
-      <div
-    className="absolute left-0 top-0 w-[45%] h-full bg-no-repeat  bg-left"
-    style={{ backgroundImage: `url(${vectorImg})` }}
-  ></div>
+<IssuesManagement></IssuesManagement>
 
-  {/* right side */}
-  <div
-    className="absolute right-0 top-0 w-[45%] h-full bg-no-repeat bg-right "
-    style={{ 
-      backgroundImage: `url(${vectorImg})`, 
-      transform: 'scaleX(-1)' 
-    }}
-  ></div>
-  {/* text */}
-  <div className="absolute inset-0 flex flex-col items-center justify-center z-30 text-white">
-    <h2 className='font-semibold text-3xl'>Resolved</h2>
-    <p className='font-semibold text-2xl'>0</p>
-  </div>
-  </div>
-</div>
-</Container>
-=======
->>>>>>> d49f98647d7159136ac108c7792890e0ebd23053
+{/* card section */}
+ <section
+          class="grid grid-cols-1 md:grid-cols-12 gap-5 w-[95%] mx-2 min-h-screen"
+        >
+          
+        </section>
 <Footer></Footer>
     </>
   )
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Container = ({children}) => {
   return (
-<<<<<<< HEAD
+
     <div className='container max-w-[1440px] mx-auto'>{children}</div>
-=======
-    <div className='container max-w-[1320px] mx-auto'>{children}</div>
->>>>>>> d49f98647d7159136ac108c7792890e0ebd23053
+
+
   );
 };
 
